5cc3caa5e2ac9d5efdee5aaab0c80e9ee365885a
NBender8668 <61422838+NBender8668@users.noreply.github.com>
2020-07-09 10:40:53 -0600

    First commit for game.

--- Engine/Object/Actor.cpp
+++ Engine/Object/Actor.cpp
@@ -1,22 +1,30 @@
 #include "pch.h"
 #include "Actor.h"
- 
+#include <fstream>
 namespace nc 
 {
 	bool nc::Actor::Load(const std::string& filename)
 	{
 		bool success = false;
 
+		std::ifstream stream(filename);
 		// open stream
 	 // check if stream is open
-		
+		if (stream.is_open())
+		{
+			success = true;
+
+			stream >> m_transform;
+		};
 
 		 // set success to true
 		 // stream >> into transform
-
-		std::string shapename;
-		stream >> shapename;
-		m_shape.Load(shapename);
+		while (!stream.eof())
+		{
+			std::string shapename;
+			stream >> shapename;
+			m_shape.Load(shapename);
+		};
 
 		return success;
 
